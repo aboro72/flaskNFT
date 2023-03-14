098e38da151451a71978b5c08b89b507f3632143
Andreas Borowczak <andreas.borowczak@googlemail.com>
2023-03-14 11:28:52 +0100

    Standart Flask

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
+
 import datetime, calendar
-from flask import Flask,url_for
+from flask import Flask,url_for, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return url_for('show_user_profile', username='Andreas')
+    return 'Leere Seite'
 
 
 @app.route('/username/<username>')
@@ -20,6 +20,15 @@ def show_user_profile(username):
 def show_post(post_id):
     # show the post
     return 'Post %d' % post_id
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_site():
+    if request.method == 'POST':
+        return 'username is: ' + request.values["username"]
+    else:
+        return '<form action="/login" method="post"><input type=t"ext" name="username"/><p><button type="submit">Submit</button>'
+
 
 
 @app.route('/hello_time')
