1628842af4bd9698859709aeac1c41747e1ab508
Andreas Borowczak <andreas.borowczak@googlemail.com>
2023-03-15 14:28:03 +0100

    Neue Funktionen

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
+import logging
+from logging.handlers import RotatingFileHandler
 import os
 
 import datetime, calendar
-from flask import Flask, url_for, request, render_template, redirect, flash
+from flask import (Flask,
+                   url_for,
+                   request,
+                   render_template,
+                   redirect,
+                   flash,
+                   make_response,
+                   session
+                   )
 
 app = Flask(__name__)
 
+
 # Standart html Flask vorlage
-
-
-@app.route('/')
-def index():
-    return 'Leere Seite'
-
-
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    time = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-    return render_template('hello1.html', name_template=name, time=time)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -25,12 +24,23 @@ def login_site():
     error = None
     if request.method == 'POST':
         if valid_login(request.form["username"], request.form["password"]):
-            flash('You were logged in')
-            return redirect(url_for('welcome', username=request.form.get("username")))
+            flash('Succesfully logged in')
+            session['username'] = request.form.get("username")
+            return redirect(url_for('welcome'))
+
         else:
             error = "Invalid username or password"
-
+            app.logger.warning('incorrect username or password for user (%s)',
+                               request.form.get("username"))
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+
+    session.pop('username', None)
+
+    return redirect(url_for('login_site'))
 
 
 def valid_login(username, password):
@@ -40,11 +50,15 @@ def valid_login(username, password):
         return False
 
 
-@app.route('/welcome/<username>')
-def welcome(username):
+@app.route('/')
+def welcome():
     time = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-    return render_template('welcome.html', username=username, time=time)
 
+    if 'username' in session:
+        app.logger.info('user (%s) logged in ' + time, session['username'])
+        return render_template('welcome.html', username=session['username'], time=time)
+    else:
+        return redirect(url_for('login_site'))
 
 # ----------------------------------------------------------------
 #
@@ -90,4 +104,9 @@ if __name__ == '__main__':
     host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
     app.debug = True
+    app.secret_key = os.urandom(24)
+    # logging
+    handler = RotatingFileHandler('site.log', maxBytes=10240, backupCount=1)
+    handler.setLevel(logging.WARNING)
+    app.logger.addHandler(handler)
     app.run(host=host, port=port)
